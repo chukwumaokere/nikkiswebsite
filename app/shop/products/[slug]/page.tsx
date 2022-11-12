130c87a27c764229868b013cd07b12d83a540cdd
chukwumaokere <chukwumaokere@yahoo.com>
2022-11-11 23:18:30 -0600

    Having a page with a client component that loads causes page to take time to load instead of just the component

--- app/shop/products/[slug]/page.tsx
+++ app/shop/products/[slug]/page.tsx
@@ -1,5 +1,93 @@
+import Header from '@/app/Header';
+import Subheader from '@/app/Subheader';
+import prisma from '@/lib/db';
+import Image from 'next/image';
+import { use } from 'react';
+import Breadcrumbs from '@/app/Breadcrumbs';
+import ParagraphText from '@/app/ParagraphText';
+import Link from 'next/link';
+import ImageWithCarousel from './ImageWithCarousel';
 import BuySection from './BuySection';
 
-export default function Page () {
-    return (<><BuySection /></>);
+async function getProduct (slug: string) {
+    const product = await prisma.product.findFirst({
+        where: {
+            slug: slug,
+        },
+    });
+    return product;
+}
+
+async function getRelatedProducts (slug: string) {
+    const products = await prisma.product.findMany(
+        {
+            where: {
+                slug: {
+                    not: slug,
+                },
+            },
+        }
+    );
+    return products;
+}
+
+type Props = {
+    params: {
+        slug: string;
+    }
+}
+
+export default function Page ({params}: Props) {
+
+    const product = use(getProduct(params.slug));
+    const relatedProducts = use(getRelatedProducts(params.slug));
+
+    return (<>
+        <div className='mb-2'>
+            <Breadcrumbs />
+        </div>
+        <div className='flex lg:flex-row gap-4 mb-2'>
+            <div className='flex flex-col lg:flex-row gap-4'>
+                <div className='flex lg:flex-col w-full h-full'>
+                    <ImageWithCarousel images={product?.images} productName={product?.name} productImage={product?.productImage?.url}>
+                        <div className='flex lg:flex-col gap-4'>
+                            <div className='flex lg:flex-col'>
+                                <Subheader>Artist&quot;s Descrption</Subheader>
+                                <ParagraphText>Just some thoughts about this product</ParagraphText>
+                            </div>
+                            <div className='flex lg:flex-col'>
+                                <Subheader>Materials Used</Subheader>
+                                <ParagraphText>Google Chrome OMEGALUL</ParagraphText>
+                            </div>
+                            <div className='flex lg:flex-col'>
+                                <Subheader>Related Products</Subheader>
+                                <div className='flex flex-col lg:flex-row gap-2 h-full'>
+                                    {relatedProducts.map((product) => (
+                                        <Link key={product.id} href={`/shop/products/${product.slug}`} className='relative w-28 h-28'>
+                                            <Image
+                                                style={{objectFit: 'contain'}}
+                                                sizes='(max-width: 768px) 100vw, 50vw'
+                                                priority={true}
+                                                fill={true}
+                                                alt={`${product.name} product image`}
+                                                src={product.productImage?.url || ''} />
+                                        </Link>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </ImageWithCarousel>
+                </div>
+            </div>
+            <div className='flex lg:flex-col gap-2'>
+                <Header>
+                    {product?.name}
+                </Header>
+                <Subheader>
+                    {product?.description}
+                </Subheader>
+                <BuySection />
+            </div>
+        </div>
+    </>);
 }
