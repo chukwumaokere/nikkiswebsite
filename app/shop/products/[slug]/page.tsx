8cffe692ca116480e2640f23d5f0ade3f8ef9b6e
chukwumaokere <chukwumaokere@yahoo.com>
2022-11-12 15:37:47 -0600

    Update with suspense boundary

--- app/shop/products/[slug]/page.tsx
+++ app/shop/products/[slug]/page.tsx
@@ -1,5 +1,7 @@
+import { Suspense } from 'react';
 import BuySection from './BuySection';
 
 export default function Page () {
-    return (<><span>Something static loading beside a loading client component</span><BuySection /></>);
+    return (<><span>Something static loading beside a loading client component</span>
+        <Suspense fallback={<div>Loading Buy Section...</div>}><BuySection /></Suspense></>);
 }
